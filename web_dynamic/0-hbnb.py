4dd67b7993c66e206c5e1e443fe1ef3fd7ba5e4a
adonijah01 <adonijahsingoei@gmail.com>
2023-11-03 13:57:30 +0300

    Cash only

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template
+app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/0-hbnb/', strict_slashes=False)
+def hbnb():
+    cache_id = str(uuid.uuid4())
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda city: city.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda amenity: amenity.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda place: place.name)
+
+    return render_template('0-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
