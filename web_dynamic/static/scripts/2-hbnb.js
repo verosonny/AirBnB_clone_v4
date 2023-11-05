9a04c4a013ca6ce4f4ed6beff9a411faf75b167e
omazoz <oumaymamazoz12@gmail.com>
2023-11-05 15:48:57 +0100

    hbnb_task4

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,21 +1,38 @@
-/* Script that listens for changes on each input checkbox*/
-$('document').ready(function () {
-  const url = 'http://0.0.0.0:5001/api/v1/status/';
-  $.get(url, function (data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
+$(document).ready(function () {
+  const amenityIds = {};
+  $('input[type="checkbox"]').change(function () {
+    if (this.checked) {
+      amenityIds[this.dataset.id] = this.dataset.name;
     } else {
-      $('#api_status').removeClass('available');
+      delete amenityIds[this.dataset.id];
     }
+    $('.amenities h4').text(Object.values(amenityIds).sort().join(', '));
+    $('.amenities h4').css({
+      width: '100%',
+      height: '100%',
+      'white-space': 'nowrap',
+      overflow: 'hidden',
+      'text-overflow': 'ellipsis',
+      'margin-left': '15%',
+      'margin-top': '0',
+      'padding-top': '10px',
+      'padding-bottom': '5px',
+      'margin-bottom': '20px',
+      'font-weight': '400',
+      'font-size': '14px'
+    });
   });
 
-  const amenities = {};
-  $('INPUT[type="checkbox"]').change(function () {
-    if ($(this).is(':checked')) {
-      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else {
-      delete amenities[$(this).attr('data-id')];
+  $.ajax({
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    dataType: 'json',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
     }
-    $('.amenities H4').text(Object.values(amenities).join(', '));
   });
 });
